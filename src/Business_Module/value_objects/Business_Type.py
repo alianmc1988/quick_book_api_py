54a0c7908d10bc6f13003433c8ef1efcdaf0df16
Alian <alianmc1988@gmail.com>
2025-03-07 13:11:07 -0300

    fix: remove hotels and bedroom from types

--- src/Business_Module/value_objects/Business_Type.py
+++ src/Business_Module/value_objects/Business_Type.py
@@ -3,7 +3,6 @@ from enum import Enum
 
 class Business_Type_Enum(str, Enum):
     BAR = "bar"
-    HOTEL = "hotel"
     RESTAURANT = "restaurant"
 
 
--- src/Business_Module/value_objects/Space_Type.py
+++ src/Business_Module/value_objects/Space_Type.py
@@ -3,5 +3,4 @@ from enum import Enum
 
 class Space_Type_Enum(str, Enum):
     TABLE = "table"
-    BEDROOM = "bedroom"
     CHAIR = "chair"
